d57c19c08adf254b026927bd0a21c0086ff3e289
Sanuja Methmal <methmal66@yahoo.com>
2021-10-12 07:32:33 +0530

    instructions to change gcc mode

--- other/radians/main.c
+++ other/radians/main.c
@@ -1,3 +1,10 @@
+/*
+  by default your ide use the c++98 mode of gcc. but it does not support some
+  newer features change settings in devc++ ide to use the latest mode of gcc
+  compiler
+  https://stackoverflow.com/questions/16951376/how-to-change-mode-from-c98-mode-in-dev-c-to-a-mode-that-supports-c0x-ran
+*/
+
 #include <stdio.h>
 
 float find_radian_value(float);
