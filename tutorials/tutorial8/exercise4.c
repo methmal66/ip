e99e86e6faa82516fa6aba1adf8882cc003b40d5
Sanuja Methmal <methmal66@yahoo.com>
2021-10-12 09:04:20 +0530

    dynamic number of user inputs

--- tutorials/tutorial8/exercise4.c
+++ tutorials/tutorial8/exercise4.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  const short length = 6;
-  const short bar_chart[] = {10, 5, 6, 7, 8, 2};
+  // input non predefined number of user inputs, assuming maximum is 1000
+  short length = 0;
+  short bar_chart[6];
+  printf(
+      "Enter values for the bar chart(enter a negative number to exit) : \n");
+  while (1) {
+    int value;
+    printf("next value>>> ");
+    scanf("%d", &value);
+    if (value < 0) {
+      printf("\nStoped getting user inputs\n\n");
+      break;
+    }
+    bar_chart[length++] = value;
+  }
 
   for (int i = 0; i < length; i++) {
     const short current_value = bar_chart[i];
