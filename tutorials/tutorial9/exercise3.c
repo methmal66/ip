4585b3b2d32675e77e1ab6c42d387f389113dd6b
Sanuja Methmal <methmal66@yahoo.com>
2021-10-18 23:58:51 +0530

    store values directly in array elements

--- tutorials/tutorial9/exercise3.c
+++ tutorials/tutorial9/exercise3.c
@@ -8,21 +8,19 @@ typedef struct {
 
 int main(int argc, char *argv[]) {
   const short SALES_PERSONS = 4, PRODUCTS_PER_PERSON = 5;
-  Sale Sales[SALES_PERSONS][PRODUCTS_PER_PERSON];
+  Sale sales[SALES_PERSONS][PRODUCTS_PER_PERSON];
 
   // get data for each Sale
   for (int i = 0; i < SALES_PERSONS; i++) {
     printf("======Sales person %hu======\n", i + 1);
     for (int j = 0; j < PRODUCTS_PER_PERSON; j++) {
       printf("------Product %hu------\n", j + 1);
-      Sale tempory_Sale;
       printf("Sales person number>> ");
-      scanf("%d", &tempory_Sale.sales_person_no);
+      scanf("%d", &sales[i][j].sales_person_no);
       printf("Product number>> ");
-      scanf("%d", &tempory_Sale.product_no);
+      scanf("%d", &sales[i][j].product_no);
       printf("Product value>> ");
-      scanf("%f", &tempory_Sale.value);
-      Sales[i][j] = tempory_Sale;
+      scanf("%f", &sales[i][j].value);
       printf("\n");
     }
     printf("\n");
@@ -32,7 +30,7 @@ int main(int argc, char *argv[]) {
   float total = 0;
   for (int i = 0; i < SALES_PERSONS; i++) {
     for (int j = 0; j < PRODUCTS_PER_PERSON; j++) {
-      total += Sales[i][j].value;
+      total += sales[i][j].value;
     }
   }
 
